37afda7865cba751a7fb7a11dd269a5bbe6d90c2
Jan Nicklas <j.nicklas@me.com>
2023-08-31 10:21:53 +0200

    fix globals

--- pack.mjs
+++ pack.mjs
@@ -4,4 +4,5 @@ await $`rm -rf dist`
 await $`ncc build --target es2020 --minify src/index.mjs`
 await $`cp node_modules/zx/build/*.d.ts dist/`
 await $`cp src/globals.mjs dist/`
+await $`cp src/globals.js dist/`
 await $`cp src/cli.mjs dist/`
