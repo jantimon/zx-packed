9b520081e56a7a3b98656f92bd0ffd0a09180e52
Jan Nicklas <j.nicklas@me.com>
2023-08-31 10:56:02 +0200

    fix globals

--- pack.mjs
+++ pack.mjs
@@ -1,8 +1,17 @@
 import "zx"
+import { readFileSync, writeFileSync } from "fs"
 
 await $`rm -rf dist`
-await $`ncc build --target es2020 --minify src/index.mjs`
+await $`ncc build --target es2020 -e "./globals.js" --minify src/index.mjs`
 await $`cp node_modules/zx/build/*.d.ts dist/`
 await $`cp src/globals.mjs dist/`
-await $`cp src/globals.js dist/`
 await $`cp src/cli.mjs dist/`
+
+const indexFiles = await glob(['dist/index.mjs', 'dist/*.index.mjs']);
+indexFiles.forEach(async (file) => {
+    // replace string import:
+    // https://github.com/google/zx/blob/956dcc3bbdd349ac4c41f8db51add4efa2f58456/src/cli.ts#L58-L59
+    // await __webpack_require__(9879)(globals);
+    // -> await import('./globals.mjs');
+    writeFileSync(file, readFileSync(file, 'utf8').replace( /__webpack_require__\(\d+\)\(globals\)/g, "import('./globals.mjs')" ));
+});
